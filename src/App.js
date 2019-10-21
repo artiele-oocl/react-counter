a945665e7eed0e5abb196c1b18b4ce66fbd9aa17
artiele-oocl <artiele@oocl.com>
2019-10-21 13:06:47 +0800

    Add Increment and Decrement methods

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      NumberIncrement: 0,
+      NumberDecrement: 0
+    };
+  }
+  increment = () => {
+    this.setState(state => {
+      return {NumberIncrement: state.NumberIncrement + 1}
+      });
+  }
+  decrement = () => {
+    this.setState(state => {
+      return {NumberDecrement: state.NumberDecrement - 1}
+      });
+  }
+  render () {
+    return (
+    <div>
+      <button onClick={this.increment}>+</button> number: {this.state.NumberIncrement}
+    <br/>
+    <button onClick={this.decrement}>-</button> number: {this.state.NumberDecrement}
     </div>
-  );
+    )}
 }
 
 export default App;
